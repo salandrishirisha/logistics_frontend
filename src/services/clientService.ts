d2b4ac4870bbbb3583be977e7658c9a52c4aa461
salandri shirisha <salandrishireesha22@gmail.com>
2026-05-09 18:33:27 +0530

    add create shipment api

--- src/services/clientService.ts
+++ src/services/clientService.ts
@@ -4,7 +4,7 @@ import  type{ Shipment } from '../types/shipment';
 
 export const clientService = {
   createShipment: (clientId: number, payload: Omit<Shipment, 'shipmentId' | 'status' | 'createdDate'>) =>
-    api.post('/client/create-shipment', payload, { params: { clientId } }),
+    api.post('/shipments', payload, { params: { clientId } }),
   myShipments: (clientId: number, page = 0, size = 10) =>
     api.get<ApiPage<Shipment>>('/client/my-shipments', { params: { clientId, page, size } }).then((r) => r.data),
 };
